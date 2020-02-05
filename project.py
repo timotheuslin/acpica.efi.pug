0f83831ac7537186ced205b4cca79c04a895daa3
Kuei-Hung Lin <kuei-line@amd.com>
2020-02-04 20:23:05 -0800

    vs2017 build

--- project.py
+++ project.py
@@ -11,7 +11,8 @@ This is the project configuration file as well the starter script for iPug.
 
 import os
 
-DEFAULT_EDK2_TAG = 'edk2-stable201905'
+DEFAULT_EDK2_TAG = 'edk2-stable201911'
+edk2_libc_path = os.path.join(os.getcwd(), 'edk2-libc')
 
 CODETREE = {
     'acpica'            : {
@@ -26,12 +27,13 @@ CODETREE = {
 
     # edk2-libc is a new edk2 repo since edk2-stable201905. StdLib resides in this repo.
     'edk2-libc'    : {
-        'path'          : os.path.join(os.path.expanduser('~'), '.cache', 'pug', 'edk2-libc'),
+        'path'          : edk2_libc_path,
         'source'        : {
             'url'       : 'https://github.com/tianocore/edk2-libc.git',
             'signature' : '6168716',   # 61687168fe02ac4d933a36c9145fdd242ac424d1 @ Apr/25/2019
         },
         'multiworkspace': True,
+        'patch'         : 'git apply --directory=%s edk2-libc.patch' % 'edk2-libc',
     },
 }
 CODETREE['acpica-efi'] = {
@@ -44,10 +46,12 @@ CODETREE['source-tools'] = {
     }
 
 if __name__ == '__main__':
+    import os
     import sys
     sys.dont_write_bytecode = True      # To inhibit the creation of .pyc file
 
+    os.environ['MSVC_TAG']='VS2017'
     PKG_DSC = 'efi/AcpiPkg_stdlib.dsc'
-    IPUG_CMD = 'ipug {0} -p {1}'.format(' '.join(sys.argv[1:]), PKG_DSC)
+    IPUG_CMD = 'python -m ipug {0} -p {1}'.format(' '.join(sys.argv[1:]), PKG_DSC)
     print(IPUG_CMD)
     os.system(IPUG_CMD)
